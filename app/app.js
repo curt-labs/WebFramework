9033a17cf7c80acc17ecb9ca4edbddd75a854772
ninnemana <ninnemana@gmail.com>
2012-10-25 17:45:07 -0500

    converted over to expressJS v3.0

--- app/app.js
+++ app/app.js
@@ -4,37 +4,35 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
 
-var app = module.exports = express.createServer();
-
-// Configuration
+var app = express();
 
 app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
-
 app.get('/', routes.index);
+app.get('/users', user.list);
 
-var port = process.env.PORT || 3000;
-
-app.listen(port);
-console.log("Application is serving on port " + port);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
